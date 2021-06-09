635575b900420b7ba438bd52872ca39b03aba51e
Reza Mohammad Darojad <RezaMohammad.Darojad@mitrais.com>
2021-06-09 13:47:11 +0700

    Update UnauthorizedException.java
    Change unauthorized exception message to Invalid Account Number/PIN

--- src/com/mitrais/atm_simulation/exception/UnauthorizedException.java
+++ src/com/mitrais/atm_simulation/exception/UnauthorizedException.java
@@ -4,7 +4,7 @@ public class UnauthorizedException extends Exception {
 	private static final long serialVersionUID = -4217313606759233468L;
 
 	public UnauthorizedException() {
-		super("User Unauthorized");
+		super("Invalid Account Number/PIN");
 	}
 
 	public UnauthorizedException(String message) {
